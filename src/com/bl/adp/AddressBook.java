d3ffe02887d9fc0d88fe1626a392bd887c609479
riya40 <priyanunna20@gmail.com>
2021-12-15 22:14:43 +0530

    Added:Additing details in contacts

--- src/com/bl/adp/AddressBook.java
+++ src/com/bl/adp/AddressBook.java
@@ -4,12 +4,49 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         //Displaying the Weelcome Message
         System.out.println("Welcome to Address Book Program");
 
-
+        AddressBook addressBook = new AddressBook();
+        addressBook.addcontactdetails();
     }
 
+    //Adding the details
+    public void addcontactdetails() {
+        ContactPerson contactPerson = new ContactPerson();
+        System.out.println("enter the details");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter first name");
+        contactPerson.firstname = scanner.next();
+        System.out.println("enter last name");
+        contactPerson.lastname = scanner.next();
+        System.out.println("enter address");
+        contactPerson.address = scanner.next();
+        System.out.println("enter city");
+        contactPerson.city = scanner.next();
+        System.out.println("enter state");
+        contactPerson.state = scanner.next();
+        System.out.println("enter pin code");
+        contactPerson.pincode = scanner.nextInt();
+        System.out.println("enter mobile number");
+        contactPerson.phonenumber = scanner.nextInt();
+        System.out.println("enter email id");
+        contactPerson.email = scanner.next();
 
+        Contacts contacts = new Contacts();
+        contacts.add(contactPerson);
+
+        System.out.println("The contact details:");
+        System.out.println("first name:" + contactPerson.firstname);
+        System.out.println("Last name:" + contactPerson.lastname);
+        System.out.println("Address:" + contactPerson.address);
+        System.out.println("City:" + contactPerson.city);
+        System.out.println("Pin code" + contactPerson.pincode);
+        System.out.println("State:" + contactPerson.state);
+        System.out.println("Phone Number:" + contactPerson.phonenumber);
+        System.out.println("Email:" + contactPerson.email);
+
+
+    }
 }
